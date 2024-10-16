48f368159e7a108b5e25846fce2565f836318475
Basil Crow <me@basilcrow.com>
2024-10-16 10:59:11 -0700

    Clean up formatting

--- src/main/resources/jenkins/plugins/extracolumns/DisableProjectColumn/button.js
+++ src/main/resources/jenkins/plugins/extracolumns/DisableProjectColumn/button.js
@@ -1,10 +1,12 @@
-Behaviour.specify(".ec-toggle-project", "extra-column-toggle-project", 0, function(e) {
-  e.onclick = function() {
+Behaviour.specify(".ec-toggle-project", "extra-column-toggle-project", 0, function (e) {
+  e.onclick = function () {
     const request = e.dataset.request;
-    fetch(request,{
-      method: 'post',
-      headers: crumb.wrap({'Content-Type': 'application/x-www-form-urlencoded'})
+    fetch(request, {
+      method: "post",
+      headers: crumb.wrap({
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
     });
-    setTimeout('window.location.reload()', 1000)
-  }
-})
+    setTimeout("window.location.reload()", 1000);
+  };
+});
