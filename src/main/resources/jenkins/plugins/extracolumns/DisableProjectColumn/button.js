2f209eaddec831b3a389b1f3c77fffb7067b2e94
Daniel Beck <daniel-beck@users.noreply.github.com>
2025-11-24 16:28:26 +0100

    [JENKINS-76307] Improve CSP compatibility, responsiveness

--- src/main/resources/jenkins/plugins/extracolumns/DisableProjectColumn/button.js
+++ src/main/resources/jenkins/plugins/extracolumns/DisableProjectColumn/button.js
@@ -7,7 +7,8 @@ Behaviour.specify(".ec-toggle-project", "extra-column-toggle-project", 0, functi
       headers: crumb.wrap({
         "Content-Type": "application/x-www-form-urlencoded",
       }),
+    }).then(function () {
+      window.location.reload();
     });
-    setTimeout("window.location.reload()", 1000);
   });
 });
