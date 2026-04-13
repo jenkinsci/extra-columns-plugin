c554c08e690d76f851d770561b2d557b0129ba1c
novinxy <grzesiekn1001@gmail.com>
2026-04-14 00:19:47 +0200

    add correct styling

--- src/main/resources/jenkins/plugins/extracolumns/LastBuildNodeColumn/showNodes.js
+++ src/main/resources/jenkins/plugins/extracolumns/LastBuildNodeColumn/showNodes.js
@@ -1,17 +1,18 @@
 Behaviour.specify(".ec-list__button button", "ec-list__button", 0, function(button) {
   button.onclick = function() {
     let tr = button.closest("tr");
-    let table = button.closest("table");
+    console.log(button)
+    let container = button.closest("#ec-last-node-container");
     let rows = tr.querySelectorAll(".ec-hidden");
     for (row of rows) {
       row.classList.toggle("jenkins-hidden");
     }
     if (button.dataset.hidden === "true") {
       button.dataset.hidden = "false";
-      button.setAttribute("tooltip", table.dataset.hideText);
+      button.setAttribute("tooltip", container.dataset.hideText);
     } else {
       button.dataset.hidden = "true";
-      button.setAttribute("tooltip", table.dataset.showText);
+      button.setAttribute("tooltip", container.dataset.showText);
     }
     Behaviour.applySubtree(tr);
   }
